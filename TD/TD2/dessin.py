13b95d2b4a400c9e821c45543685d7de53f3e2f8
manon123-design <manon.pinar@gmail.com>
2025-03-03 15:57:45 +0100

    td

---
+++ TD/TD2/dessin.py
@@ -0,0 +1,12 @@
+import tkinter as tk
+import random as rd 
+
+def dessine_cercle() :
+    centre_x= rd.randint(50)
+
+
+racine = tk.Tk()
+
+racine.title('Mon dessin')
+bouton_cercle=tk.Bouton
+racine.mainloop()
